b10c5d399a28f4fc686b48f55b87e89b410d5fca
Fp19se <filipe.s.almeida6@ba.estudante.senai.br>
2026-09-17 20:29:17 -0300

    Atualizar o 10-atividade_pontuada.py

--- 10-atividade_pontuada.py
+++ 10-atividade_pontuada.py
@@ -1,24 +1,34 @@
 import os
 os.system("cls")
 
-
-
 gasolina = "G"
-alcool = 'A'
-combustivel = input('Informe o tipo de combustivel (A - Alcool / G - Gasolina): ')
-quantidade = float(input('Infome a quantidade de litros vendidos: '))
+alcool = "A"
+
+combustivel = input('Informe o tipo de combustivel (A - Alcool / G - Gasolina): ').upper()
+quantidade = float(input('Informe a quantidade de litros que deseja: '))
+
 litro_gasolina = 6.59
 litro_alcool = 3.79
 
-if 'A' == combustivel:
+if combustivel == alcool:
     if quantidade <= 25:
-        desconto = litro_alcool * 0.10
-else:
-    desconto = litro_alcool * 0.10
+        desconto = 0.10
+    else:
+        desconto = 0.20
 
-if 'G' == combustivel:
+    total = quantidade * litro_alcool
+
+elif combustivel == gasolina:
     if quantidade <= 25:
-        desconto = litro_gasolina * 0.15
-else:
-    desconto = litro_gasolina * 0.30
+        desconto = 0.15
+    else:
+        desconto = 0.30
 
+    total = quantidade * litro_gasolina
+
+valor_desconto = total * desconto
+valor_a_pagar = total - valor_desconto
+
+print('\nValor total: R$', total)
+print('Desconto: R$', valor_desconto)
+print('Valor a pagar: R$', valor_a_pagar)
