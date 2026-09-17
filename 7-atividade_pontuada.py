5f508cb5f1c04896a5343030990887b1b8d68600
Fp19se <filipe.s.almeida6@ba.estudante.senai.br>
2026-09-17 20:41:22 -0300

    Atualizar o 7-atividade_pontuada.py

--- 7-atividade_pontuada.py
+++ 7-atividade_pontuada.py
@@ -3,23 +3,22 @@ os.system('cls')
 
 produto = str(input('Qual o produto você deseja comprar: '))
 quant = int(input('Qual a quantidade adquirida: '))
-preco = int(input('Qual o preço do produto: '))
-
-
-if quant <= 5:
-    desconto = preco * 0.02
-elif quant > 5 and quant <= 10:
-    desconto = preco * 0.03
-else:
-    desconto = preco * 0.05
-
+preco = float(input('Qual o preço do produto: '))
 
 total = quant * preco
+
+if quant <= 5:
+    desconto = total * 0.02
+elif quant > 5 and quant <= 10:
+    desconto = total * 0.03
+else:
+    desconto = total * 0.05
+
 total_a_pagar = total - desconto
 
 print('\nSeu produto é: ', produto)
 print('A quantidade escolhida: ', quant)
-print('O preco do produto: ', preco)
-print('O total deu: ', total)
-print('O desconto é: ', desconto,'%')
-print('O valor descontado é: ', total_a_pagar)
+print('O preco do produto: R$', preco)
+print('O total deu: R$', total)
+print('O desconto é: R$', desconto)
+print('O valor a pagar é: R$', total_a_pagar)
